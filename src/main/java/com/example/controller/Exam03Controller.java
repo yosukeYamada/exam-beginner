3d3f8d683ae3aa338ab8fb9adb737b266f149bda
yosukeYamada <yamada_141@yahoo.co.jp>
2019-11-05 14:30:39 +0900

    exam3を追加

---
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sun.glass.ui.Application;
+
+@Controller
+@RequestMapping("/ex03")
+public class Exam03Controller {
+
+	@Autowired
+	private ServletContext application;
+
+	private static final double tax = 1.1;
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam03";
+	}
+
+	@RequestMapping("/buy")
+	public String buy(Integer item1, Integer item2, Integer item3) {
+		int totalPrice = item1 + item2 + item3;
+		application.setAttribute("totalPrice", totalPrice);
+		application.setAttribute("totalPriceWithTax", (int)(totalPrice * tax));		
+		return "exam03-result";
+	}
+
+}
